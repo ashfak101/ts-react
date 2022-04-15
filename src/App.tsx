b73dbd15e41a3259457b9bb2e34a43f6b4b9c483
Md Ashfakul Karim Rahi <ashfakulkarim2@gmail.com>
2022-04-15 15:12:28 +0600

    PersonList

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@
 import './App.css';
 import Great from './components/Greet';
 import Person from './components/Person';
+import PersonList from './components/PersonList';
 
 
 function App() {
-  const personName={
-    first :'Bruce',
-    last : 'Wayne' , 
+  const personName = {
+    first: 'Bruce',
+    last: 'Wayne',
   }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Clark',
+      last: 'Kent'
+    },
+    {
+      first: '',
+      last: 'Wayne'
+    },
+  ]
   return (
     <div className="App">
-        <Great name='Hamdan' messageCount={10} isLoggedIn={false}/>
-        <Person  name={personName}/>
+      <Great name='Hamdan' messageCount={10} isLoggedIn={true} />
+      <Person name={personName} />
+      <PersonList names={nameList}/>
     </div>
   );
 }
